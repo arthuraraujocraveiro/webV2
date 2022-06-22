aea5fd5add2ab2827037c912b9367ce4fabd9c3f
unknown <arthur.craveiro@sounidesc.com.br>
2022-06-22 13:24:32 -0300

    commit da prova

---
+++ disciplina/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Disciplina(models.Model):
+
+    nome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    disciplina = models.CharField(max_length=100)
---
+++ professor/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Professor(models.Model):
+
+    nome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
